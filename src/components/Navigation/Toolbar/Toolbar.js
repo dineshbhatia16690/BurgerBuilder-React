8c6b88381e3c2872a9932816d5f826d38ead5435
Dinesh Bhatia <dinebhat@amazon.com>
2020-09-19 13:46:41 -0700

    Adding a toolbar

---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import classes from './Toolbar.css';
+const toolbar = (props) => (
+    // eslint-disable-next-line no-unused-expressions
+    <header className={classes.Toolbar}>
+        <div>MENU</div>
+        <div>LOGO</div>
+        <nav>
+            ...
+        </nav>
+    </header>
+);
+
+export default toolbar;
