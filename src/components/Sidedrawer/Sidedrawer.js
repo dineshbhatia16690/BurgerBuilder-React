43747b87862a8638bfdaf97be4dbf9bae27717f2
Dinesh Bhatia <dinebhat@amazon.com>
2020-09-23 16:56:53 -0700

    Changing folder structure

--- src/components/Sidedrawer/Sidedrawer.js
+++ src/components/Sidedrawer/Sidedrawer.js
@@ -4,7 +4,7 @@ import classes from './Sidedrawer.css';
 import Logo from '../Logo/Logo'
 import NavigationItems from '../Navigation/NavigationItems/NavigationItems';
 import Backdrop from '../UI/Backdrop/Backdrop';
-import Aux from '../../hoc/Aux';
+import Aux from '../../hoc/Aux/Aux';
 
 const sideDrawer = (props) => {
     let attachedClasses = [classes.SideDrawer, classes.Close];
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,7 +1,7 @@
 import React, {Component} from "react";
 
 import classes from './Modal.css'
-import Aux from '../../../hoc/Aux';
+import Aux from '../../../hoc/Aux/Aux';
 import Backdrop from '../Backdrop/Backdrop';
 
 // We could have wrapped the export with React.memo which internally uses shouldComponentHook
