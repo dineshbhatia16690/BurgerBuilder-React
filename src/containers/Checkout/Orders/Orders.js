2b19dbf9112d08010c14aa813781230dd81959a8
Dinesh Bhatia <dinebhat@amazon.com>
2020-11-03 13:29:31 -0800

    Fetching the Orders

--- src/containers/Checkout/Orders/Orders.js
+++ src/containers/Checkout/Orders/Orders.js
@@ -1,8 +1,36 @@
 import React, { Component } from "react";
 
 import Order from '../../../components/Order/Order';
+import axiosInstance from "../../../axios-orders";
+import globalErrorHandler from "../../../GlobalErrorHandler/GlobalErrorHandler";
 
 class Orders extends Component {
+
+    state = {
+        orders: [],
+        loading: true
+    }
+
+    componentDidMount() {
+        console.log('In Orders ...');
+        const fetchedOrders = [];
+        axiosInstance.get('/orders.json').then(
+            res => {
+                for (let key in res) {
+                    fetchedOrders.push({
+                        ...res.data[key],
+                        id: key
+                    });
+                }
+                this.setState({orders: fetchedOrders, loading: false});
+                console.log(res);
+            }
+        ).catch(err => {
+            this.setState({loading: false});
+            console.log(err);
+        })
+    }
+
     render() {
         return (
             <div>
@@ -13,4 +41,4 @@ class Orders extends Component {
     }
 }
 
-export default Orders;
+export default globalErrorHandler(Orders, axiosInstance);
