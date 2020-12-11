1400877fb347e468e0c886d95395c8187dd5a158
Dinesh Bhatia <dinebhat@amazon.com>
2020-12-10 16:14:16 -0800

    Changing the Order of our ingredients manually

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -4,10 +4,10 @@ import BuildControl from '../BuildControls/BuildControl/BuildControl';
 import classes from './BuildControls.css'
 
 const controls = [
-    {label: 'Salad', type: 'salad'},
     {label: 'Bacon', type: 'bacon'},
+    {label: 'Cheese', type: 'cheese'},
     {label: 'Meat', type: 'meat'},
-    {label: 'Cheese', type: 'cheese'}
+    {label: 'Salad', type: 'salad'}
 ];
 
 const buildControls = (props) => (
